e216de33ab71f66b5bb804eff420f01bd5ace7a9
Harshikaa Agrawal <agrawalharshikaa@gmail.com>
2021-08-30 10:36:35 +0530

    Started formalizing games.

---
+++ Game.v
@@ -0,0 +1,33 @@
+Require Import Coq.Program.Equality.
+Require Import Coq.btauto.Btauto.
+Require Import Coq.Lists.Streams.
+From Coq.ssr Require Import ssreflect ssrfun ssrbool.
+From mathcomp.ssreflect Require Import seq eqtype.
+
+Set Boolean Equality Schemes.
+Set Decidable Equality Schemes.
+
+Inductive player := player_O | player_P.
+Definition other_player (p : player) : player
+:= match p with
+   | player_O => player_P
+   | player_P => player_O
+   end.
+
+Record strict_game
+:= { possible_moves : Type
+   ; first_player : player
+   ; plays_won_by_P : Stream possible_moves -> Prop }.
+
+Definition position (g : strict_game) : Type
+:= seq (possible_moves g).
+
+Search "even" (nat -> bool).
+Definition next_player {g} (p : position g) : player
+:= if Nat.even (length p) then first_player g else other_player (first_player g).
+
+Definition strategy {g} (p : player) : Type
+:= forall (pos : position g) (h : next_player pos = p), possible_moves g.
+
+
+
