444facafe8f6513dc475950efaafb310c1dc368e
Harshikaa Agrawal <52704397+harshikaaagrawal@users.noreply.github.com>
2021-09-03 23:41:23 +0530

    Update tic_tac_toe.v

--- tic_tac_toe.v
+++ tic_tac_toe.v
@@ -1,6 +1,7 @@
 Require Import Coq.Program.Equality.
 Require Import Coq.btauto.Btauto.
 Require Import Coq.Lists.Streams.
+Require Import Coq.Strings.Strings.
 From Coq.ssr Require Import ssreflect ssrfun ssrbool.
 From mathcomp.ssreflect Require Import seq eqtype.
 
@@ -12,6 +13,7 @@ Axiom later : forall {T}, T.
 Definition row (board : seq (seq player)) : bool := later.
 Definition column (board : seq (seq player)) : bool := later.
 Definition diagonal (board : seq (seq player)) : bool := later.
-Definition output_board(board : seq (seq player)) : ___ := later.
-Definition game_result(current_player : nat) (board : seq (seq player)) (moves_played : nat) : ___ := later.
-Definition game_intro _____ : ____ := later.
+Definition output_board (board : seq (seq player)) : string := later.
+Definition game_result (current_player : nat) (board : seq (seq player)) (moves_played : nat) : player := later.
+Definition game_result_string (p : player) : string := later.
+Definition game_intro : string := later.
