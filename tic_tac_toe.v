133ec5cea20df492bca87dccf62832bd647d7be1
Harshikaa Agrawal <52704397+harshikaaagrawal@users.noreply.github.com>
2021-09-03 22:01:34 +0530

    Create tic_tac_toe.v

---
+++ tic_tac_toe.v
@@ -0,0 +1,8 @@
+Require Import Coq.Program.Equality.
+Require Import Coq.btauto.Btauto.
+Require Import Coq.Lists.Streams.
+From Coq.ssr Require Import ssreflect ssrfun ssrbool.
+From mathcomp.ssreflect Require Import seq eqtype.
+
+Set Boolean Equality Schemes.
+Set Decidable Equality Schemes.
