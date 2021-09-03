34be291099de3fd3a7b44fb6cb3666a08cb30887
Harshikaa Agrawal <52704397+harshikaaagrawal@users.noreply.github.com>
2021-09-03 23:32:53 +0530

    Update tic_tac_toe.v

--- tic_tac_toe.v
+++ tic_tac_toe.v
@@ -10,4 +10,8 @@ Set Decidable Equality Schemes.
 Inductive player := player_1 | player_2 | empty.
 Axiom later : forall {T}, T.
 Definition row (board : seq (seq player)) : bool := later.
-
+Definition column (board : seq (seq player)) : bool := later.
+Definition diagonal (board : seq (seq player)) : bool := later.
+Definition output_board(board : seq (seq player)) : ___ := later.
+Definition game_result(current_player : nat) (board : seq (seq player)) (moves_played : nat) : ___ := later.
+Definition game_intro _____ : ____ := later.
