9373eaff3652e4731cce1013a5f38c9f5af47bac
Harshikaa Agrawal <52704397+harshikaaagrawal@users.noreply.github.com>
2021-09-04 00:25:10 +0530

    Update tic_tac_toe.v

--- tic_tac_toe.v
+++ tic_tac_toe.v
@@ -8,12 +8,17 @@ From mathcomp.ssreflect Require Import seq eqtype.
 Set Boolean Equality Schemes.
 Set Decidable Equality Schemes.
 
-Inductive player := player_1 | player_2 | empty.
+Inductive player := player_1 | player_2.
 Axiom later : forall {T}, T.
-Definition row (board : seq (seq player)) : bool := later.
+Definition cell := option player.
+Definition board := seq (seq cell).
+Definition row (b: board) : bool := later.
 Definition column (board : seq (seq player)) : bool := later.
 Definition diagonal (board : seq (seq player)) : bool := later.
 Definition output_board (board : seq (seq player)) : string := later.
-Definition game_result (current_player : nat) (board : seq (seq player)) (moves_played : nat) : player := later.
-Definition game_result_string (p : player) : string := later.
+Definition game_result (current_player : player) (board : seq (seq player)) (moves_played : nat) : option player := later.
+Definition game_result_string (p : option player) : string := later.
 Definition game_intro : string := later.
+Definition main_game (board : seq (seq player)) : unit := later.
+Definition input_and_make_move (current_player : player) (board : seq (seq player)): player*seq (seq player) := later.
+Definition make_move (board : seq (seq player)) (current_player : player) (r : nat) (c : nat) : bool * seq (seq player) := later.
