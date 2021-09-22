8ca26a0f09c501205b3349804c9e4115ddd61dc2
Harshikaa Agrawal <52704397+harshikaaagrawal@users.noreply.github.com>
2021-09-22 21:56:31 +0530

    Update nim.cpp

--- nim.cpp
+++ nim.cpp
@@ -1,7 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int other_player(int current_player)
+{
+    return abs(current_player - 1);
+}
+
+void game_result(int current_player)
+{
+    cout<<other_player(current_player)+1<<"\n";
+}
+
+void play_nim(int n, int p[], int x)
+{
+    int current_player = 0;
+    
+    while(x > 0)
+    {
+        cout<<"Player "<<current_player+1<<"\n";
+        int pile, amount;
+        cin>>pile>>amount;
+        if(p[pile-1] >= amount)
+        {
+            p[pile-1] -= amount;
+            x -= amount;
+        }
+        else
+        {
+            break;
+        }
+        current_player = other_player(current_player);
+    }
+    
+    game_result(current_player);
+}
+
+void take_input()
+{
     int n;
     cin>>n;
     int p[n];
@@ -12,25 +47,10 @@ int main() {
         x+=p[i];
     }
     
-    int current_player = 0;
-    
-    while(x > 0)
-    {
-        cout<<"Player "<<current_player+1<<"\n";
-        int pile, amount;
-        cin>>pile>>amount;
-        if(p[pile-1] >= amount)
-        {
-            p[pile-1]-=amount;
-            x -= amount;
-        }
-        else
-        {
-            break;
-        }
-        current_player = abs(current_player - 1);
-    }
-    
-    cout<<abs(current_player - 1) +1;
+    play_nim(n, p, x);
+}
+
+int main() {
+    take_input();
 	return 0;
 }
